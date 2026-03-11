559e3578a975690f27c91bb5ebc479b42c78ef93
Felipe Caicedo <luis.caicedo.dev7@gmail.com>
2026-03-10 23:09:59 -0500

    first commit

---
+++ src/main/java/api/hub/domain/usuario/validaciones/ValidadorDeUsuario.java
@@ -0,0 +1,11 @@
+package api.hub.domain.usuario.validaciones;
+
+
+import api.hub.domain.topico.RegistroTopicoDTO;
+import api.hub.domain.usuario.RegistroUsuarioDTO;
+import api.hub.domain.usuario.RespuestaUsuarioDTO;
+
+public interface ValidadorDeUsuario {
+
+    public void validate(RegistroUsuarioDTO RegistroUsuarioDTO);
+}
